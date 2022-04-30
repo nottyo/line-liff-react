990fdf4cee92fa8f9cfa55ffbfe7d1bfe012368f
traitanit-h <traitanit.h@linecorp.com>
2022-04-30 17:31:26 +0700

    add cypress tests

--- cypress/integration/line-login.spec.js
+++ cypress/integration/line-login.spec.js
@@ -1,14 +1,40 @@
-describe('Line Login', () => {
-  it('do line login', () => {
-    // given
-    cy.visit(Cypress.config('baseUrl'));
-    // when
-    cy.get('[data-testid="login"]').click();
-    cy.origin('https://access.line.me', () => {
-      cy.log('in secondary origin');
-      cy.get('input[type="text"]').type('melodythqaios@gmail.com');
-      cy.get('input[type="password"]').type('thqaat1234!');
-      cy.get('button[type="submit"]').click();
+describe('Line Login Example Test', () => {
+  describe('login without session', () => {
+    beforeEach(() => {
+      // do login before each test case so number of login = number of test cases
+      cy.lineLoginWithoutSession(Cypress.env('email'), Cypress.env('password'));
+    });
+
+    it('render profileUrl', () => {
+      cy.get('img[alt="profileUrl"]').should('be.visible');
+    });
+
+    it('render displayName', () => {
+      cy.get('[data-testid="displayName"]').should('have.text', 'displayName: Nott');
+    });
+
+    it('render logout button', () => {
+      cy.get('[data-testid="logout"]').should('be.visible');
+    });
+  });
+
+  describe('login with session', () => {
+    beforeEach(() => {
+      // login once for all test cases and preserve its login session.
+      cy.lineLoginWithSession(Cypress.env('email'), Cypress.env('password'));
+      cy.visit(Cypress.config('baseUrl'));
+    });
+
+    it('render profileUrl', () => {
+      cy.get('img[alt="profileUrl"]').should('be.visible');
+    });
+
+    it('render displayName', () => {
+      cy.get('[data-testid="displayName"]').should('have.text', 'displayName: Nott');
+    });
+
+    it('render logout button', () => {
+      cy.get('[data-testid="logout"]').should('be.visible');
     });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,51 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('lineLoginWithoutSession', (email, password) => {
+  // v1 without session cache
+  const args = { email, password };
+  cy.intercept({
+    method: 'POST',
+    url: 'https://api.line.me/oauth2/v2.1/token',
+  }).as('createToken');
+  cy.visit(Cypress.config('baseUrl'));
+  cy.get('[data-testid="login"]').click();
+  cy.origin('https://access.line.me', { args }, ({ email, password }) => {
+    cy.get('input[type="text"]').type(email);
+    cy.get('input[type="password"]').type(password);
+    cy.get('button[type="submit"]').click();
+  });
+  // confirm that login is success!
+  cy.wait('@createToken').its('response.statusCode').should('eq', 200);
+});
+
+Cypress.Commands.add('lineLoginWithSession', (email, password) => {
+  // v2 with session cache
+  const args = { email, password };
+  cy.intercept({
+    method: 'POST',
+    url: 'https://api.line.me/oauth2/v2.1/token',
+  }).as('createToken');
+  cy.session(
+    // use email, password as a session cache key
+    args,
+    () => {
+      cy.visit(Cypress.config('baseUrl'));
+      cy.get('[data-testid="login"]').click();
+      cy.origin('https://access.line.me', { args }, ({ email, password }) => {
+        cy.get('input[type="text"]').type(email);
+        cy.get('input[type="password"]').type(password);
+        cy.get('button[type="submit"]').click();
+      });
+      // confirm that login is success!
+      cy.wait('@createToken').its('response.statusCode').should('eq', 200);
+    },
+    {
+      validate: () => {
+        const accessTokenKey = `LIFF_STORE:${Cypress.env('LIFF_ID')}:accessToken`;
+        expect(localStorage.getItem(accessTokenKey)).to.not.be.empty;
+      },
+    },
+  );
+});
